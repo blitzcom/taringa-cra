b1a9e96c4eb1c67ec51d98b63e4d8d3ea10075aa
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-20 17:45:51 -0600

    Update deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -11,7 +11,10 @@ sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xzf heroku-linux-amd64.tar.gz -C /usr/local/lib
 sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
-git remote -v
+cat >> ~/.ssh/config << EOF
+  VerifyHostKeyDNS yes
+  StrictHostKeyChecking no
+EOF
 
 # Deploy
 git push heroku $CIRCLE_SHA1:refs/heads/master
