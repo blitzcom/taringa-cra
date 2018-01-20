ffa791717e5be74076a4f396fadf58662a81e6e4
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-20 17:23:10 -0600

    Update deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -20,6 +20,12 @@ sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xzf heroku-linux-amd64.tar.gz -C /usr/local/lib
 sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
+
 git remote add heroku git@heroku.com:$APP_NAME.git
 git remote -v
 
