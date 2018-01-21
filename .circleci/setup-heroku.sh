56b80bfd7e488a85be9a190fe7c2c6a2a7c6b772
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-20 18:18:22 -0600

    Cambios menores (#5)

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
+echo "Deploy to $APP_NAME app."
+
 git remote add heroku git@heroku.com:$APP_NAME.git
 
-wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+# Install Heroku CLI
+echo "Install Heroku CLI"
+wget -nv https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
-sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo tar -xzf heroku-linux-amd64.tar.gz -C /usr/local/lib
 sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
-cat > ~/.netrc << EOF
-  machine api.heroku.com
-    login $HEROKU_LOGIN
-    password $HEROKU_API_KEY
-  machine git.heroku.com
-    login $HEROKU_LOGIN
-    password $HEROKU_API_KEY
-EOF
-
-cat >> ~/.ssh/config << EOF
-VerifyHostKeyDNS yes
-StrictHostKeyChecking no
-EOF
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
