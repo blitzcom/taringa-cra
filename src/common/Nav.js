8986c5a26527528d96ad3f22fcf8d2d3eddb1f41
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-21 22:18:56 -0600

    Agregar Nav
    - Instalar Storybook para crear componentes de React

---
+++ src/common/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Nav.css'
+
+const Nav = props => (
+  <div className="navbar navbar-dark Nav" >
+    <a className="navbar-brand font-weight-bold" href="/">TARINGA!</a>
+  </div>
+)
+
+export default Nav
