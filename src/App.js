7ab6a04b33e0f266da17aec293cd7941f01ef7ed
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-21 18:07:58 -0600

    Agregar react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
 import logo from './logo.svg'
 import './App.css'
+
+const Home = props => (
+  <p className="App-intro">
+    To get started, edit <code>src/App.js</code> and save to reload.
+  </p>
+)
+
+const Post = props => (
+  <p className="App-intro">
+    Post page demo.
+  </p>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/post" component={Post}/>
+        </div>
+      </Router>
     )
   }
 }
