bf6105af5fbfa19fabc8a65d3d549aac212a3119
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-21 19:03:18 -0600

    Instalar bootstrap 4

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
+
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-
 import configure from './store'
 
 const store = configure()
