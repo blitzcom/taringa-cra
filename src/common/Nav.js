8b5ab1cae5865db81d9498b3bb37e5c431644ada
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-21 23:17:25 -0600

    Corregir lint

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -3,8 +3,10 @@ import React from 'react'
 import './Nav.css'
 
 const Nav = props => (
-  <div className="navbar navbar-dark Nav" >
-    <a className="navbar-brand font-weight-bold" href="/">TARINGA!</a>
+  <div className="navbar navbar-dark Nav">
+    <a className="navbar-brand font-weight-bold" href="/">
+      TARINGA!
+    </a>
   </div>
 )
 
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
+import { storiesOf } from '@storybook/react'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import Nav from '../common/Nav'
 
-storiesOf('Nav', module).add('default', () => <Nav/>)
+storiesOf('Nav', module).add('default', () => <Nav />)
