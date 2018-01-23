5a95d8d6e23c666e986fb4add431bb94e4471a06
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-23 00:30:27 -0600

    Agregar estados para Score

--- src/stories/components/Score.js
+++ src/stories/components/Score.js
@@ -1,32 +1,55 @@
 import React from 'react'
+import classNames from 'classnames'
 
+import './Score.css'
 import { humanizeNum } from '../../Utils'
 
-const Score = props => (
-  <div className="Story-score-display">
-    <button
-      className="Story-vote"
-      onClick={() => props.onVoteUp()}
-    >
-      <i className="fa fa-arrow-up"/>
-    </button>
-    <div className="Story-score">
-      { humanizeNum(props.upvotes - props.downvotes) }
+const Score = props => {
+  const classVoteUpButton = classNames('Score-vote', {
+    'Score-up': props.voted > 0,
+  })
+
+  const classVoteDownButton = classNames('Score-vote', {
+    'Score-down': props.voted < 0,
+  })
+
+  const classVotes = classNames('Score-votes', {
+    'Score-down': props.voted < 0,
+    'Score-up': props.voted > 0,
+  })
+
+  return (
+    <div className="Score-display">
+      <button
+        className={classVoteUpButton}
+        disabled={props.isVoting}
+        onClick={() => props.onVoteUp()}
+      >
+        <i className="fa fa-arrow-up"/>
+      </button>
+
+      <div className={classVotes}>
+        { humanizeNum(props.upvotes - props.downvotes) }
+      </div>
+
+      <button
+        className={classVoteDownButton}
+        disabled={props.isVoting}
+        onClick={() => props.onVoteDown()}
+      >
+        <i className="fa fa-arrow-down"/>
+      </button>
     </div>
-    <button
-      className="Story-vote"
-      onClick={() => props.onVoteDown()}
-    >
-      <i className="fa fa-arrow-down"/>
-    </button>
-  </div>
-)
+  )
+}
 
 Score.defaultProps = {
   downvotes: 0,
+  isVoting: false,
   onVoteDown: () => {},
   onVoteUp: () => {},
   upvotes: 0,
+  voted: 0,
 }
 
 export default Score
--- src/storybook/index.js
+++ src/storybook/index.js
@@ -8,10 +8,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../fontawesome-all.css'
 import '../css/action.css'
 
-import Nav from '../common/Nav'
-
 import { StoryShout } from './data'
+
+import Nav from '../common/Nav'
 import Story from '../stories/components/Story'
+import Score from '../stories/components/Score'
 
 storiesOf('Nav', module).add('default', () => <Nav />)
 
@@ -24,3 +25,70 @@ storiesOf('Story', module).add('shout', () => {
     />
   )
 })
+
+storiesOf('Score', module)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    const downvotes = text('Downvotes', 2500)
+    const upvotes = text('Upvotes', 5500)
+
+    return (
+      <Score
+        downvotes={downvotes}
+        onVoteDown={action('onVoteDown')}
+        onVoteUp={action('onVoteUp')}
+        upvotes={upvotes}
+      />
+    )
+  })
+
+storiesOf('Score', module)
+  .addDecorator(withKnobs)
+  .add('voted up', () => {
+    const downvotes = text('Downvotes', 2500)
+    const upvotes = text('Upvotes', 5500)
+
+    return (
+      <Score
+        downvotes={downvotes}
+        onVoteDown={action('onVoteDown')}
+        onVoteUp={action('onVoteUp')}
+        upvotes={upvotes}
+        voted={1}
+      />
+    )
+  })
+
+storiesOf('Score', module)
+  .addDecorator(withKnobs)
+  .add('voted down', () => {
+    const downvotes = text('Downvotes', 2500)
+    const upvotes = text('Upvotes', 5500)
+
+    return (
+      <Score
+        downvotes={downvotes}
+        onVoteDown={action('onVoteDown')}
+        onVoteUp={action('onVoteUp')}
+        upvotes={upvotes}
+        voted={-1}
+      />
+    )
+  })
+
+storiesOf('Score', module)
+  .addDecorator(withKnobs)
+  .add('is voting', () => {
+    const downvotes = text('Downvotes', 2500)
+    const upvotes = text('Upvotes', 5500)
+
+    return (
+      <Score
+        downvotes={downvotes}
+        isVoting
+        onVoteDown={action('onVoteDown')}
+        onVoteUp={action('onVoteUp')}
+        upvotes={upvotes}
+      />
+    )
+  })
