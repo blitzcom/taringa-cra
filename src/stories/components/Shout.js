4084b0173365b05c5b399d3284883e1f23e3e524
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-24 15:48:47 -0600

    Fix Shout
    Corregir error de thumbnail cuando no hay imagenes que mostrar.

--- src/stories/components/Shout.js
+++ src/stories/components/Shout.js
@@ -4,57 +4,63 @@ import TimeAgo from 'react-timeago'
 import './Shout.css'
 import { humanizeNum, esFormatter } from '../../Utils'
 
-const Shout = props => (
-  <div className="Shout">
-    <a className="Shout-thumbnail" href="/">
-      <img
-        src={props.summary.images.slice[0].url}
-        alt="Thumbnail"
-      />
-    </a>
-    <div className="Shout-info">
-      <a
-        className="Shout-title text-dark"
-        href={`/story/${props.slug}`}
-      >
-        { props.summary.excerpt }
+const Shout = props => {
+  return (
+    <div className="Shout">
+      <a className="Shout-thumbnail" href="/">
+        {
+          props.summary.images.amount > 0 && (
+            <img
+              src={props.summary.images.slice[0].url}
+              alt="Thumbnail"
+            />
+          )
+        }
       </a>
-      <p className="Shout-meta">
-        Posteado por
-        <a href={`/user/${props.owner.username}`}>
-          {props.owner.username}
+      <div className="Shout-info">
+        <a
+          className="Shout-title text-dark"
+          href={`/story/${props.slug}`}
+        >
+          { props.summary.excerpt }
         </a>
-        <TimeAgo
-          date={props.created}
-          formatter={esFormatter}
-        />
-      </p>
-      <div className="Shout-actions">
-        <button
-          className="btn btn-action"
-          title="Expandir"
-        >
-          <i className="fa fa-expand"/>
-        </button>
+        <p className="Shout-meta">
+          Posteado por
+          <a href={`/user/${props.owner.username}`}>
+            {props.owner.username}
+          </a>
+          <TimeAgo
+            date={props.created}
+            formatter={esFormatter}
+          />
+        </p>
+        <div className="Shout-actions">
+          <button
+            className="btn btn-action"
+            title="Expandir"
+          >
+            <i className="fa fa-expand"/>
+          </button>
 
-        <button
-          className="btn btn-action"
-          title="Comentarios"
-        >
-          <i className="fa fa-comments"/>
-          {humanizeNum(props.comments)} comentarios
-        </button>
+          <button
+            className="btn btn-action"
+            title="Comentarios"
+          >
+            <i className="fa fa-comments"/>
+            {humanizeNum(props.comments)} comentarios
+          </button>
 
-        <button
-          className="btn btn-action"
-          title="Compartir"
-        >
-          <i className="fa fa-retweet"/>
-          {humanizeNum(props.shares)}
-        </button>
+          <button
+            className="btn btn-action"
+            title="Compartir"
+          >
+            <i className="fa fa-retweet"/>
+            {humanizeNum(props.shares)}
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Shout
