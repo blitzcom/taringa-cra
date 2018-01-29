1d4da2928416827a80dad9d43ee160f0bd3ea680
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-28 19:00:03 -0600

    Eliminar archivos obsoletos
    - Eliminar reducers de stories-comments.
    
    - Remover componente Paginator. Reemplazado por Infinite Scroll.

--- src/stories-comments/types.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_REQUEST = 'stories-comments/FETCH_REQUEST'
-export const FETCH_SUCCESS = 'stories-comments/FETCH_SUCCESS'
-export const FETCH_FAILURE = 'stories-comments/FETCH_FAILURE'
-
-export const CREATE_FETCH_CONTROl = 'stories-comments/CREATE_FETCH_CONTROl'
