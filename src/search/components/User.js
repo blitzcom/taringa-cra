8e5ee3bcd98e0425aa6dafc28a98db9b93f57475
Diego Lopez Garcia <aud0l@hotmail.com>
2018-02-16 18:52:07 -0600

    Trucar nombre de usuario en componente user de búsqueda

--- src/search/components/User.js
+++ src/search/components/User.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { humanizeNum } from '../../Utils'
+
 const User = props => {
+  const fullname = `${props.firstname} ${props.lastname}`
+
   return (
     <div className="col-6 mb-4">
       <div className="card">
-        <div className="card-body p-3 d-flex">
+        <div className="card-body p-2 d-flex">
           <img
             src={props.avatar}
             className="rounded mr-3"
@@ -19,18 +23,18 @@ const User = props => {
               width: 68,
             }}
           />
-          <div>
-            <h6 className="m-0">
-              {props.firstname} {props.lastname}
+          <div style={{ overflow: 'hidden' }}>
+            <h6 className="m-0 text-truncate" title={fullname}>
+              {fullname}
             </h6>
             <Link to={`/u/${props.username}`} style={{ fontSize: '90%' }}>
               @{props.username}
             </Link>
 
             <p className="mb-0 mt-1 text-muted small">
-              {props.followers} seguidores &nbsp;
-              {props.following} siguiendo &nbsp;
-              {props.stories} posts
+              {humanizeNum(props.followers)} seguidores &nbsp;
+              {humanizeNum(props.following)} siguiendo &nbsp;
+              {humanizeNum(props.stories)} posts
             </p>
           </div>
         </div>
