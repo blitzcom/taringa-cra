fa904da0113fa599ff9f15e9e4f0d447f8d426dd
Diego Lopez Garcia <aud0l@hotmail.com>
2018-02-26 17:00:58 -0600

    Agregar menu principal y sección para login

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -15,6 +15,29 @@ const Nav = props => (
         <div className="form-inline">
           <SearchInput />
         </div>
+
+        <div className="navbar-nav ml-5 mr-auto">
+          <a className="nav-item nav-link active font-weight-bold" href="/">
+            MI T!
+          </a>
+
+          <a className="nav-item nav-link font-weight-bold" href="/">
+            GLOBAL
+          </a>
+
+          <a className="nav-item nav-link font-weight-bold" href="/">
+            CANALES
+          </a>
+        </div>
+
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" href="/">
+            Iniciar sesión
+          </a>
+          <a className="nav-item nav-link" href="/">
+            Crear cuenta
+          </a>
+        </div>
       </div>
     </div>
   </div>
