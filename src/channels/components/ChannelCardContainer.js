3ae51b23985f44acec3abd5a857f22e766c702ba
Diego Lopez Garcia <aud0l@hotmail.com>
2018-03-03 04:05:06 -0600

    Update ChannelCardContainer.js

--- src/channels/components/ChannelCardContainer.js
+++ src/channels/components/ChannelCardContainer.js
@@ -4,39 +4,7 @@ import { connect } from 'react-redux'
 
 import CardDecorator from '../../common/CardDecorator'
 import withResource from '../../HOC/Resource'
-import { fetchTrigger } from '../actions'
-
-export const ChannelCard = ({ channel, control }) => {
-  return (
-    <CardDecorator
-      avatar={channel.thumbnail}
-      cover={channel.background}
-      placeholder={control.status === 'fetching'}
-    >
-      <div className="card-body">
-        <h5 className="card-title">{channel.title}</h5>
-
-        {channel.description !== channel.title && (
-          <p className="card-text mt-3 mb-4">{channel.description}</p>
-        )}
-
-        <button className="btn btn-primary btn-block font-weight-bold mb-3">
-          SEGUIR
-        </button>
-
-        <p className="text-muted small mb-0">
-          Siguiendo este canal podrás ver sus publicaciones en tu página de
-          inicio
-        </p>
-      </div>
-    </CardDecorator>
-  )
-}
-
-ChannelCard.defaultProps = {
-  channel: {},
-  control: { status: 'fetching' },
-}
+import ChannelCard from './ChannelCard'
 
 const getId = props => props.match.params.channel
 
