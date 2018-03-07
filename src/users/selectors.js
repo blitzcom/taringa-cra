56bf5fd0b8077bb2f303ce46768339375bee679d
Diego Lopez Garcia <aud0l@hotmail.com>
2018-03-06 20:51:19 -0600

    Cambiar existencia de usuario por control

--- src/users/selectors.js
+++ src/users/selectors.js
@@ -1,14 +1,16 @@
 import { createSelector } from 'reselect'
 
 const storyState = (state, storyId) => state.entities.stories[storyId]
+const usersControlState = state => state.control.usersFetch
 const usersState = state => state.entities.users
 
 export const userControlSelector = createSelector(
   storyState,
+  usersControlState,
   usersState,
-  (story, users) => {
-    if (story && users[story.owner.username]) {
-      return { status: 'success' }
+  (story, controls, users) => {
+    if (story && controls[story.owner.username]) {
+      return controls[story.owner.username]
     }
 
     return { status: 'fetching' }
