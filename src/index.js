3a7f6531942b6dbb690ae685b0ded4f8b08f8de2
Diego Lopez Garcia <aud0l@hotmail.com>
2018-03-08 21:36:24 -0600

    Instalar jquery para bootstrap js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import $ from 'jquery'
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import './fontawesome-all.css'
 import './index.css'
@@ -13,6 +15,8 @@ import registerServiceWorker from './registerServiceWorker'
 import configure from './store'
 import { restoreItemSize } from './settings/actions'
 import { add as addFlash } from './flash/actions'
+
+window.$ = $
 
 const store = configure()
 restoreItemSize(store)
