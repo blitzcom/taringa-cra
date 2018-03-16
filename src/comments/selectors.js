f7cd274d520cfc5d791c070d8679cc524f4b9a1e
Diego Lopez Garcia <aud0l@hotmail.com>
2018-03-16 16:09:17 -0600

    Agregar selector a comentarios

--- src/comments/selectors.js
+++ src/comments/selectors.js
@@ -1,54 +1,9 @@
 import _ from 'lodash'
 import { createSelector } from 'reselect'
 
-const commentsState = state => state.entities.comments
-const commentsFetchState = (state, storyId) =>
-  state.control.commentsFetch[storyId]
+const fetchState = (state, ownProps) =>
+  state.control.commentsFetch[ownProps.storyId]
 
-export const commentsStatusSelector = createSelector(
-  commentsFetchState,
-  state => {
-    if (state) {
-      return _.assign({}, state, {
-        hasMoreContent: state.ids.length < state.totalCount,
-      })
-    }
-
-    return {
-      status: 'fetching',
-      error: '',
-      ids: [],
-      totalCount: 0,
-      hasMoreContent: true,
-    }
-  }
-)
-
-export const commentsSelector = createSelector(
-  commentsFetchState,
-  commentsState,
-  (control, comments) => {
-    if (control) {
-      const denormalizedComments = _.map(control.ids, i => {
-        const currentComment = comments[i]
-
-        const denormalizedReplies = _.map(
-          currentComment.replies.items,
-          r => comments[r]
-        )
-
-        const mergedDenormalizedReplies = _.assign({}, currentComment, {
-          replies: _.assign({}, currentComment.replies, {
-            items: denormalizedReplies,
-          }),
-        })
-
-        return mergedDenormalizedReplies
-      })
-
-      return denormalizedComments
-    }
-
-    return []
-  }
-)
+export const commentsSelector = createSelector(fetchState, control => {
+  return control || { status: 'fetching', items: [], totalCount: 0 }
+})
