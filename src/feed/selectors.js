86f8343618df01d52753b470089f741e8315ca89
Diego Lopez Garcia <aud0l@hotmail.com>
2018-03-18 20:55:12 -0600

    Fix: cambiar de tamaño de sumario

--- src/feed/selectors.js
+++ src/feed/selectors.js
@@ -1,8 +1,14 @@
+import _ from 'lodash'
 import { createSelector } from 'reselect'
 
-const feedState = (state, ownProps) => state.feed[ownProps.feedId]
+const feedState = (state, ownProps) =>
+  state.feed[ownProps.feedId] || { status: 'fetching', ids: [], totalCount: 0 }
+const sizeState = state => state.settings.itemSize
 
 export const summariesSelector = createSelector(
+  sizeState,
   feedState,
-  feed => feed || { status: 'fetching', ids: [], totalCount: 0 }
+  (size, feed) => {
+    return _.assign({ size }, feed)
+  }
 )
