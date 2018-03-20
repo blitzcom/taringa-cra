ebe0001377f8bd08429ac3f24228691615b8f069
Diego Lopez Garcia <aud0l@hotmail.com>
2018-03-19 21:52:07 -0600

    Nuevo componente Tab
    Este componente se encarga de mostrar los mensajes y contenido de una
    categoía de resultado de búsqueda.

---
+++ src/search/components/Tab.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react'
+
+import './Tab.css'
+
+class Tab extends PureComponent {
+  static defaultProps = {
+    status: 'success',
+    totalCount: null,
+  }
+
+  render() {
+    const { status, className, totalCount, children } = this.props
+
+    const classes = className || 'card-body'
+    const isFetching = status === 'fetching'
+    const hasSuccess = status === 'success'
+    const hasFailure = status === 'failure'
+    const isInitial = hasSuccess && totalCount === null
+    const hasResults = hasSuccess && totalCount !== null && totalCount > 0
+    const isEmpty = hasSuccess && totalCount !== null && totalCount === 0
+
+    return (
+      <div className={classes}>
+        {isFetching && (
+          <p className="Tab-slate">
+            <i className="fa fa-circle-notch fa-spin mr-1" />
+            Buscando...
+          </p>
+        )}
+
+        {isInitial && (
+          <p className="Tab-slate">Puedes buscar posts, usuarios o canales</p>
+        )}
+
+        {isEmpty && <p className="Tab-slate">No hay resultados</p>}
+
+        {hasResults && children}
+
+        {hasFailure && (
+          <p className="Tab-slate text-danger">
+            ¡Ratas! Intenta recargar la página
+          </p>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Tab
