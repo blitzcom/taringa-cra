21b4dbe330aa875c7c73f707738d8b37ef85ffed
Diego Lopez Garcia <aud0l@hotmail.com>
2018-04-06 19:13:17 -0500

    Agregar tests faltantes a comments

--- src/comments/__tests__/sagas.js
+++ src/comments/__tests__/sagas.js
@@ -3,7 +3,12 @@ import sagaHelper from 'redux-saga-testing'
 import { call, put, select } from 'redux-saga/effects'
 
 import Taringa from '../../api'
-import { loadComments, getCommentsControl } from '../sagas'
+import {
+  fetchReplies,
+  getCommentsControl,
+  getRepliesControl,
+  loadComments,
+} from '../sagas'
 
 describe('Load comments saga', () => {
   describe('(a) loads with success', () => {
@@ -50,7 +55,51 @@ describe('Load comments saga', () => {
     })
   })
 
-  describe('(b) loads with failure', () => {
+  describe('(b) loads next cursor', () => {
+    const id = 'a'
+    const it = sagaHelper(loadComments({ id }))
+
+    it('selects control', result => {
+      expect(result).toEqual(select(getCommentsControl, id))
+
+      return { after: 'a' }
+    })
+
+    it('puts fetch request action', result => {
+      expect(result).toEqual(
+        put({
+          type: 'comments/FETCH_REQUEST',
+          id: id,
+        })
+      )
+    })
+
+    it('calls api', result => {
+      expect(result).toEqual(call(Taringa.story.comments, id, { after: 'a' }))
+
+      return { id, items: [{ id: 2 }], before: 3 }
+    })
+
+    it('puts fetch success action', result => {
+      expect(result).toEqual(
+        put({
+          type: 'comments/FETCH_SUCCESS',
+          entities: {
+            comments: { 2: { id: 2 } },
+            commentRoot: { [id]: { id, items: [2], before: 3 } },
+          },
+          id: id,
+          result: id,
+        })
+      )
+    })
+
+    it('ends', result => {
+      expect(result).toBeUndefined()
+    })
+  })
+
+  describe('(c) loads with failure', () => {
     const id = 'b'
     const it = sagaHelper(loadComments({ id }))
 
@@ -78,6 +127,131 @@ describe('Load comments saga', () => {
       expect(result).toEqual(
         put({
           type: 'comments/FETCH_FAILURE',
+          id: id,
+          message: 'Network Error',
+        })
+      )
+    })
+  })
+})
+
+describe('Fetch replies saga', () => {
+  describe('(a) loads with success', () => {
+    const id = 'a'
+    const it = sagaHelper(fetchReplies({ id }))
+
+    it('selects control', result => {
+      expect(result).toEqual(select(getRepliesControl, id))
+
+      return {}
+    })
+
+    it('puts fetch request action', result => {
+      expect(result).toEqual(
+        put({
+          type: 'comments/FETCH_REPLIES_REQUEST',
+          id: id,
+        })
+      )
+    })
+
+    it('calls api', result => {
+      expect(result).toEqual(call(Taringa.comment.byId, id, {}))
+
+      return { id, replies: [], before: 3 }
+    })
+
+    it('puts fetch success action', result => {
+      expect(result).toEqual(
+        put({
+          type: 'comments/FETCH_REPLIES_SUCCESS',
+          entities: {
+            comments: { [id]: { before: 3, id, replies: id } },
+            replyRoots: { [id]: { status: 'success' } },
+          },
+          id: id,
+          result: id,
+        })
+      )
+    })
+
+    it('ends', result => {
+      expect(result).toBeUndefined()
+    })
+  })
+
+  describe('(b) loads next cursor', () => {
+    const id = 'a'
+    const it = sagaHelper(fetchReplies({ id }))
+
+    it('selects control', result => {
+      expect(result).toEqual(select(getRepliesControl, id))
+
+      return { after: 'a' }
+    })
+
+    it('puts fetch request action', result => {
+      expect(result).toEqual(
+        put({
+          type: 'comments/FETCH_REPLIES_REQUEST',
+          id: id,
+        })
+      )
+    })
+
+    it('calls api', result => {
+      expect(result).toEqual(call(Taringa.comment.byId, id, { after: 'a' }))
+
+      return { id, replies: [], before: 3 }
+    })
+
+    it('puts fetch success action', result => {
+      expect(result).toEqual(
+        put({
+          type: 'comments/FETCH_REPLIES_SUCCESS',
+          entities: {
+            comments: { [id]: { before: 3, id, replies: id } },
+            replyRoots: { [id]: { status: 'success' } },
+          },
+          id: id,
+          result: id,
+        })
+      )
+    })
+
+    it('ends', result => {
+      expect(result).toBeUndefined()
+    })
+  })
+
+  describe('(c) loads with failure', () => {
+    const id = 'b'
+    const it = sagaHelper(fetchReplies({ id }))
+
+    it('selects control', result => {
+      expect(result).toEqual(select(getRepliesControl, id))
+      return {}
+    })
+
+    it('puts fetch request action', result => {
+      expect(result).toEqual(
+        put({
+          type: 'comments/FETCH_REPLIES_REQUEST',
+          id: id,
+        })
+      )
+    })
+
+    it('calls api', result => {
+      expect(result).toEqual(call(Taringa.comment.byId, id, {}))
+
+      return new Error('Network Error')
+    })
+
+    it('puts fetch failure action', result => {
+      expect(result).toEqual(
+        put({
+          type: 'comments/FETCH_REPLIES_FAILURE',
           id: id,
           message: 'Network Error',
         })
